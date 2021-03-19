c51249f8eeada2b2a580221f101d91ce4c848f6e
Connor Fuhrman <connor.fuhrman@email.arizona.edu>
2021-03-19 11:36:54 -0700

    Initial commit. Added and tested hello_world cross compiler. Added Boost library

---
+++ cmake/test/test.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(void) {
+	printf("Hello!! Test works ... \n");
+	return 0;
+}
---
+++ cmake/test/test.cxx
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main(void) {
+	std::cout << "Hello!! Test works ... " << std::endl;
+	return 0;
+}
